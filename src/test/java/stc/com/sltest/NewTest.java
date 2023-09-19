a4f6b2d4ed17f7060f58435cca81a524b9b1f0b2
spselenim <pawal.satish@gmail.com>
2023-09-19 08:53:26 +0530

    1st

---
+++ src/test/java/stc/com/sltest/NewTest.java
@@ -0,0 +1,28 @@
+package stc.com.sltest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  try
+	  {
+	  String strChromePath="D:\\Satish\\05DemoApps\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe";	
+System.setProperty("webdriver.chrome.driver", strChromePath);
+System.out.println("TEST ______");
+WebDriver driver=new ChromeDriver();
+driver.get("file:///D:/Satish/05DemoApps/demo.html");
+driver.findElement(By.id("fname")).sendKeys("Helllllo");
+Thread.sleep(2000);
+System.out.println("DONE");
+driver.quit();
+	  }
+	  catch(Exception e)
+	  {
+		System.out.println(e.getMessage());  
+	  }
+  }
+}
