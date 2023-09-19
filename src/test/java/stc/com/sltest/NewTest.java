e90c3d674736d505a62ddd13854340648c5061e7
parth23571 <pawal.sateesh@gmail.com>
2023-09-19 20:51:00 +0530

    parth -alll

--- src/test/java/stc/com/sltest/NewTest.java
+++ src/test/java/stc/com/sltest/NewTest.java
@@ -2,24 +2,27 @@ package stc.com.sltest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class NewTest {
+import sltest2.BaseClass;
+
+public class NewTest extends BaseClass{
   @Test
   public void f() {
 	  try
 	  {
-	  String strChromePath="D:\\Satish\\05DemoApps\\chromedriver-win32\\chromedriver-win32\\chromedriver.exe";	
-System.setProperty("webdriver.chrome.driver", strChromePath);
-
+init();
 System.out.println("----------TEST ++++++______");
 System.out.println("----------TEST =====______");
 System.out.println("PARTH Comments");
-WebDriver driver=new ChromeDriver();
+
 driver.get("file:///D:/Satish/05DemoApps/demo.html");
-driver.findElement(By.id("fname")).sendKeys("Helllllo");
-driver.findElement(By.id("lname")).sendKeys("Byeeee");
+WebElement webID=getElementID("First");
+
+webID.sendKeys("Helllllo");
+getElementID1("Last").sendKeys("Byeeee");
 
 Thread.sleep(2000);
 System.out.println("DONE");
